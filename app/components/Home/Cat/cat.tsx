e1ed1db7102c06fbb36d8305f4cc855b57868136
Keerthi <rhkeerthi3@gmail.com>
2025-09-09 19:38:32 +0530

    Added Food categories file

---
+++ app/components/Home/Cat/cat.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+
+const categories = [
+    {
+        name:"Pasta"
+    },
+    {
+        name:"Pizza"
+    },
+    {
+        name:"Indian"
+    },
+    {
+        name:"chinese"
+    },
+    {
+        name:"Thai"
+    },
+    {
+        name:"Italian"
+    },
+    {
+        name:"American"
+    },
+    {
+        name:"Japanese"
+    },
+    {
+        name:"Mexican"
+    },
+    {
+        name:"Burgers"
+    },
+    {
+        name:"Desert"
+    },
+    {
+        name:"Greek"
+    }
+];
+
+function cat() {
+  return (
+    <div>cat</div>
+  )
+}
+
+export default cat
--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Hero from './Hero/Hero'
 import Res from './Res/Res'
-
+import Cat from './Cat/cat';
 function Home() {
   return (
     <div className=' h-[2000px]'>
       
       <Hero/>
       <Res/>
+      <Cat/>
     </div>
    
   )
