dd96305cbfdf416680fff025d548c59a0c6da194
Keerthi <rhkeerthi3@gmail.com>
2025-09-09 21:29:39 +0530

    Compleetd the UI of the About

--- app/components/Home/About/About.tsx
+++ app/components/Home/About/About.tsx
@@ -23,8 +23,31 @@ function About() {
 
                         <div>
                             <h1 className='text-base sm:text-lg font-extrabold'>Bold Flavors, Homemade Vibes</h1>
-                            <p className=''>At CravyKitchen, we blend bold, global flavors with the warmth of home cooking. 
+                            <p className='mt-2 text-gray-800 dark:text-gray-300 font-medium text-sm sm:text-base'>At CravyKitchen, we blend bold, global flavors with the warmth of home cooking. 
                                 Whether you're here for a quick recipe or a deep culinary dive, we’ve got your cravings covered.</p>
+                        </div>
+                        </div>
+                    </div>
+
+
+                    <div className='mt-8'>
+                        <div>
+                        <p className='text-3xl md:text-5xl  opacity-50 font-bold'>02</p>
+
+                        <div>
+                            <h1 className='text-base sm:text-lg font-extrabold'>Simple Recipes, Crave-Worthy Results</h1>
+                            <p className='mt-2 text-gray-800 dark:text-gray-300 font-medium text-sm sm:text-base'>Our recipes are designed for real life — easy-to-follow, deliciously satisfying, and made with ingredients you actually have.</p>
+                        </div>
+                        </div>
+                    </div>
+
+                    <div className='mt-8'>
+                        <div>
+                        <p className='text-3xl md:text-5xl  opacity-50 font-bold'>03</p>
+
+                        <div>
+                            <h1 className='text-base sm:text-lg font-extrabold'>Food is a Feeling</h1>
+                            <p className='mt-2 text-gray-800 dark:text-gray-300 font-medium text-sm sm:text-base'>We believe food should spark joy, connection, and nostalgia. CravyKitchen is more than a kitchen — it's a space to celebrate cravings, culture, and creativity.</p>
                         </div>
                         </div>
                     </div>
