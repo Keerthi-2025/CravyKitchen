8d9204adf78a426f29a1726626154bfb421fbb47
Keerthi <rhkeerthi3@gmail.com>
2025-09-10 14:46:50 +0530

    Modified ScrollToTop file

--- app/components/Helper/ScrollToTop.tsx
+++ app/components/Helper/ScrollToTop.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+"use client";
+
+import React, { useEffect, useState } from 'react'
 
 function ScrollToTop() {
+
+    const[isVisivle, setIsVisivle] = useState(false);
+
+    useEffect(()=>{
+        const toggleVisibility = () =>{
+            if(window.scrollY > 300 ) setIsVisivle(true);
+            else setIsVisivle(false);
+        }
+    })
   return (
     <div>ScrollToTop</div>
   )
