9272300d4f7294e0fdaaf0987c9b737c573e569b
Keerthi <rhkeerthi3@gmail.com>
2025-09-10 14:52:13 +0530

    Added Show and Hide function of  ScrollToTop file

--- app/components/Helper/ScrollToTop.tsx
+++ app/components/Helper/ScrollToTop.tsx
@@ -4,6 +4,8 @@ import React, { useEffect, useState } from 'react'
 
 function ScrollToTop() {
 
+    // show and hide functionality for button
+
     const[isVisivle, setIsVisivle] = useState(false);
 
     useEffect(()=>{
@@ -11,7 +13,21 @@ function ScrollToTop() {
             if(window.scrollY > 300 ) setIsVisivle(true);
             else setIsVisivle(false);
         }
-    })
+
+        window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll",toggleVisibility);
+    }, [])
+
+
+    //scrolltotop functionality
+    const ScrollToTop = () =>{
+        window.scroll({
+            top:0,
+            behavior:"smooth"
+        })
+    }
+    
+
   return (
     <div>ScrollToTop</div>
   )
